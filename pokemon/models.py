d9eaad77356c6ac4a2d5ad3f2acc4f3f220784db
Jorge Hinojosa <jorge@example.com>
2025-06-17 12:24:22 -0500

    Creación de modelos Entrenador y Pokemon

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Entrenador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
+class Pokemon(models.Model):
+    TIPOS = [
+        ('F', 'Fuego'),
+        ('A', 'Agua'),
+        ('P', 'Planta'),
+        ('E', 'Eléctrico'),
+    ]
+    
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=1, choices=TIPOS)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    altura = models.DecimalField(max_digits=5, decimal_places=2)
+    entrenador = models.ForeignKey(Entrenador, on_delete=models.CASCADE)
+    fotografia = models.ImageField(upload_to='pokemons/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.nombre
